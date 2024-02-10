8dcc977a7436c40847ce26906f30c1625e97aee0
nikishkaa <nikita.goutor@gmail.com>
2024-02-10 19:43:22 +0300

    Add the zero number count task.

---
+++ src/by/itstep/goutor/javastage/stage14/task/task4/controller/Main.java
@@ -0,0 +1,18 @@
+package by.itstep.goutor.javastage.stage14.task.task4.controller;
+
+import by.itstep.goutor.javastage.stage14.task.task4.model.logic.ZeroNumberCount;
+import by.itstep.goutor.javastage.stage14.task.task4.util.ZeroCountUI;
+import by.itstep.goutor.javastage.stage14.util.testarray.TestArray;
+import by.itstep.goutor.javastage.stage14.util.view.PrintStringBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = TestArray.getTestArray();
+
+        int count = ZeroNumberCount.getZeroNumberCount(numbers);
+
+
+        PrintStringBuilder.print(ZeroCountUI.getZeroCountUI(count));
+    }
+}
---
+++ src/by/itstep/goutor/javastage/stage14/task/task4/util/ZeroCountUI.java
@@ -0,0 +1,9 @@
+package by.itstep.goutor.javastage.stage14.task.task4.util;
+
+public class ZeroCountUI {
+    public static StringBuilder getZeroCountUI(int count) {
+        StringBuilder builder = new StringBuilder();
+
+        return builder.append("Zero number in your array - ").append(count);
+    }
+}

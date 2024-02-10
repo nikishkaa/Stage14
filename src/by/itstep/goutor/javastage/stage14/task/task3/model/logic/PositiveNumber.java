c70c9723aa9137d837fa096a0154614372c36f38
nikishkaa <nikita.goutor@gmail.com>
2024-02-10 19:35:11 +0300

    Add the PositiveNumber count task.

---
+++ src/by/itstep/goutor/javastage/stage14/task/task3/model/logic/PositiveNumber.java
@@ -0,0 +1,18 @@
+package by.itstep.goutor.javastage.stage14.task.task3.model.logic;
+
+public class PositiveNumber {
+    public static int getCountPositiveNumber(int[] numbers) {
+        int count = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] > 0) {
+                count++;
+            }
+
+        }
+
+
+        return count;
+    }
+}
---
+++ src/by/itstep/goutor/javastage/stage14/task/task3/util/PositiveNumberUI.java
@@ -0,0 +1,10 @@
+package by.itstep.goutor.javastage.stage14.task.task3.util;
+
+public class PositiveNumberUI {
+    public static StringBuilder getPositiveNumberUI(int count) {
+
+        StringBuilder builder = new StringBuilder();
+
+        return builder.append(count).append(" - Positive digit in your vector");
+    }
+}

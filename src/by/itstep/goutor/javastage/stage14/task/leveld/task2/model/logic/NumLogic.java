ead09b3fea4de5a70478352add988df09c80a781
nikishkaa <nikita.goutor@gmail.com>
2024-02-21 20:20:31 +0300

    New architecture task 2 level D.

--- src/by/itstep/goutor/javastage/stage14/task/leveld/task2/model/logic/NumLogic.java
+++ src/by/itstep/goutor/javastage/stage14/task/leveld/task2/model/logic/NumLogic.java
@@ -3,21 +3,11 @@ package by.itstep.goutor.javastage.stage14.task.leveld.task2.model.logic;
 public class NumLogic {
     public static int getSum(int[] numbers) {
 
+        int positionOne = getFirstPosition(numbers);
+        int positionTwo = getSecondPosition(numbers, positionOne);
+
+
         int sum = 0;
-
-        int positionOne = -1;
-        int positionTwo = -1;
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == 0) {
-                if (positionOne == -1) {
-                    positionOne = i;
-                }
-                positionTwo = i;
-            }
-
-        }
-
 
         for (int i = 0; i < numbers.length; i++) {
             if (i > positionOne && i < positionTwo) {
@@ -28,4 +18,44 @@ public class NumLogic {
 
         return sum;
     }
+
+
+    private static int getFirstPosition(int[] numbers) {
+
+        int positionOne = -1;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] == 0) {
+                if (positionOne == -1) {
+                    positionOne = i;
+                }
+            }
+
+        }
+
+        return positionOne;
+    }
+
+
+    private static int getSecondPosition(int[] numbers, int positionOne) {
+
+        int positionTwo = -1;
+
+        for (int i = numbers.length - 1; i > 0; i--) {
+
+            if (numbers[i] == 0) {
+
+                if (positionOne < i) {
+
+                    positionTwo = i;
+
+                }
+            }
+
+        }
+
+        return positionTwo;
+    }
+
 }

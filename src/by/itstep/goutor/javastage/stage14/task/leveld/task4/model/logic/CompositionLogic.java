a9019a3e7741a90e99ba7109439107d2f9c98fec
nikishkaa <nikita.goutor@gmail.com>
2024-02-21 20:51:50 +0300

    Update task 4 level D.

--- src/by/itstep/goutor/javastage/stage14/task/leveld/task4/model/logic/CompositionLogic.java
+++ src/by/itstep/goutor/javastage/stage14/task/leveld/task4/model/logic/CompositionLogic.java
@@ -2,30 +2,16 @@ package by.itstep.goutor.javastage.stage14.task.leveld.task4.model.logic;
 
 
 public class CompositionLogic {
+    public static final int IMPOSSIBLE_ELEMENT = -1;
+
     public static int getCompositionBetweenZeroElement(int[] numbers) {
 
         int composition = 1;
 
-        int firstPosition = -1;
-        int secondPosition = -1;
-        int zeroCount = 0;
+        int[] positions = getFirstAndSecondPosition(numbers);
 
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == 0) {
-                zeroCount++;
-                if (zeroCount == 1) {
-                    firstPosition = i;
-                } else {
-                    secondPosition = i;
-                    break;
-                }
-
-            }
-
-        }
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (i > firstPosition && i < secondPosition) {
+            if (i > positions[0] && i < positions[1]) {
                 composition *= numbers[i];
             }
 
@@ -33,5 +19,29 @@ public class CompositionLogic {
 
 
         return composition;
+    }
+
+
+    private static int[] getFirstAndSecondPosition(int[] numbers) {
+
+        int zeroCount = 0;
+
+        int[] positions = new int[]{IMPOSSIBLE_ELEMENT, IMPOSSIBLE_ELEMENT};
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == 0) {
+                zeroCount++;
+                if (zeroCount == 1) {
+                    positions[0] = i;
+                } else {
+                    positions[1] = i;
+                    break;
+                }
+
+            }
+
+        }
+
+        return positions;
     }
 }
